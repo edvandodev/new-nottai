80f53f1cc32cbdd0027e57a529679a4e43d05539
edfoda69 <edfoda69@gmail.com>
2025-12-17 16:13:34 +0000

    test-main

--- android/app/src/main/java/com/nottai/app/MainActivity.java
+++ android/app/src/main/java/com/nottai/app/MainActivity.java
@@ -1,5 +1,14 @@
 package com.nottai.app;
 
+import android.os.Bundle;
+import android.webkit.WebView;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WebView.setWebContentsDebuggingEnabled(true);
+    }
+}
