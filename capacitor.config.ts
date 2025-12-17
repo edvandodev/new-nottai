127e318a0028fe00adf6e225496b6dca75e25390
edfoda69 <edfoda69@gmail.com>
2025-12-17 16:06:43 +0000

    test-log

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,10 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.nottai.app',
   appName: 'nottai',
-  webDir: 'dist'
+  webDir: 'dist',
+  android: {
+    loggingBehavior: 'debug'
+  }
 };
 
 export default config;
