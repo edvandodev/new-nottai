096a1275a127384b5ec53095580eb2dfa71b312d
edfoda69 <edfoda69@gmail.com>
2025-12-17 00:49:06 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.nottai.app',
+  appName: 'nottai',
+  webDir: 'dist'
+};
+
+export default config;
