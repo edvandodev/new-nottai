4bebcdb76eb5cdb903c798873679cf1390292e3c
edfoda69 <edfoda69@gmail.com>
2025-12-17 16:43:07 +0000

    fix-android

--- android/app/src/main/java/com/nottai/app/MainActivity.java
+++ android/app/src/main/java/com/nottai/app/MainActivity.java
@@ -1,12 +1,5 @@
 package com.nottai.app;
 
 import com.getcapacitor.BridgeActivity;
-import android.webkit.WebView;
 
-public class MainActivity extends BridgeActivity {
-    @Override
-    public void onResume() {
-        super.onResume();
-        WebView.setWebContentsDebuggingEnabled(true);
-    }
-}
+public class MainActivity extends BridgeActivity {}
